38e45fd1b99ff4971ff8944d55817d8f03078c01
sai-krishna26 <saikrishnavishwakarma@gmail.com>
2026-02-24 11:27:12 +0530

    loops
    loops with some added tasks

---
+++ task 8/ThisKey.java
@@ -0,0 +1,8 @@
+class ThisKey
+{
+ThisKey()
+{
+	System.out.println(this);
+	System.out.println(this);
+}
+}
---
+++ task 8/ThisKeyRunner.java
@@ -0,0 +1,8 @@
+class ThisKeyRunner
+{
+	public static void main(String... arg)
+	{
+		ThisKey thiskey=new ThisKey();
+		ThisKey thiskey1=new ThisKey();
+	}
+}
