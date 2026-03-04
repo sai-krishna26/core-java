6de69edbc3ce4bff7df29406fa10496e9093eaf6
sai-krishna26 <saikrishnavishwakarma@gmail.com>
2026-03-04 12:01:51 +0530

    added tasks

--- task 20/TennisBall.java
+++ task 20/TennisBall.java
@@ -1,18 +1,64 @@
 class TennisBall
 {
-	void bounce(){ System.out.println("executing bounce in TennisBall"); }
-	void roll(){ System.out.println("executing roll in TennisBall"); }
-	void hit(){ System.out.println("executing hit in TennisBall"); }
-	void spin(){ System.out.println("executing spin in TennisBall"); }
-	void inflate(){ System.out.println("executing inflate in TennisBall"); }
-	void deflate(){ System.out.println("executing deflate in TennisBall"); }
-	void changeColor(){ System.out.println("executing changeColor in TennisBall"); }
-	void checkPressure(){ System.out.println("executing checkPressure in TennisBall"); }
-	void measureWeight(){ System.out.println("executing measureWeight in TennisBall"); }
-	void displayBrand(){ System.out.println("executing displayBrand in TennisBall"); }
-	void pack(){ System.out.println("executing pack in TennisBall"); }
-	void unpack(){ System.out.println("executing unpack in TennisBall"); }
-	void testQuality(){ System.out.println("executing testQuality in TennisBall"); }
-	void recycle(){ System.out.println("executing recycle in TennisBall"); }
-	void store(){ System.out.println("executing store in TennisBall"); }
+	void bounce()
+	{
+		System.out.println("executing bounce in TennisBall"); 
+	}
+
+	void roll()
+	{
+		System.out.println("executing roll in TennisBall");
+	}
+	void hit()
+	{
+		System.out.println("executing hit in TennisBall");
+	}
+	void spin()
+	{
+		System.out.println("executing spin in TennisBall"); 
+	}
+	void inflate()
+	{
+		System.out.println("executing inflate in TennisBall"); 
+	}
+	void deflate()
+	{
+		System.out.println("executing deflate in TennisBall");
+	}
+	void changeColor()
+	{
+		System.out.println("executing changeColor in TennisBall"); 
+	}
+	void checkPressure()
+	{
+		System.out.println("executing checkPressure in TennisBall"); 
+	}
+	void measureWeight()
+	{
+		System.out.println("executing measureWeight in TennisBall"); 
+	}
+	void displayBrand()
+	{
+		System.out.println("executing displayBrand in TennisBall"); 
+	}
+	void pack()
+	{
+		System.out.println("executing pack in TennisBall"); 
+	}
+	void unpack()
+	{
+		System.out.println("executing unpack in TennisBall"); 
+	}
+	void testQuality()
+	{
+		System.out.println("executing testQuality in TennisBall"); 
+	}
+	void recycle()
+	{
+		System.out.println("executing recycle in TennisBall"); 
+	}
+	void store()
+	{
+		System.out.println("executing store in TennisBall"); 
+	}
 }
