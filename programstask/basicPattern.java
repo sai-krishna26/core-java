bf4b0485f533ff7be856814d118d0fdcc14b460a
sai-krishna26 <saikrishnavishwakarma@gmail.com>
2026-03-21 08:29:33 +0530

    updated program

---
+++ programstask/basicPattern.java
@@ -0,0 +1,15 @@
+class basicPattern
+{
+	public static void main(String[] stars)
+	{
+		for(int i=1;i<=4;i++)
+		{
+			System.out.print("*");
+			for(int j=1;j<=5;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
